d8bd46ce7337f006eb6a5e63f3ac69199215c795
M-Jauharil <jojokuleok12e@gmail.com>
2025-10-16 21:36:10 +0700

    Ditambahkan asset yang akan digunakan  untuk aplikasi(font, icon, dsb). Diperbaiki formatting dan positioning halaman sign up

--- ufix_mobile/lib/main.dart
+++ ufix_mobile/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Electronics Learning App',
-      home: WelcomeUnlogged(), // Start with welcome screen
+      home: SignupScreen(), // Start with welcome screen
       routes: {
         // Authentication Flow
         '/unlogged': (context) => WelcomeUnlogged(),
